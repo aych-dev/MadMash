04b9dcbe9c8c2714140cdfbcd00a3eb3b807c7a6
aych <aychdev1@gmail.com>
2023-07-14 22:34:25 -0400

    Updated Image size

--- src/Components/ImageDisplay.tsx
+++ src/Components/ImageDisplay.tsx
@@ -3,17 +3,15 @@ import useMintlist from '../Hooks/useMintlist';
 const ImageDisplay = () => {
   const { listOfImages } = useMintlist();
 
-  if (listOfImages.length < 1) 
-    return null
-  
-  const testImage = listOfImages[0].image
- 
+  if (listOfImages.length < 1) return null;
+
+  const testImage = listOfImages[50].image;
 
   return (
     <div>
       <div className='grid grid-cols-2'>
         <div className='col-span-2'>
-          <img src={testImage} alt='test' />
+          <img className='h-auto w-48' src={testImage} alt='test' />
         </div>
       </div>
     </div>
