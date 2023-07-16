f84aa370de584391b6824977d07cc3663c10bf5a
aych <aychdev1@gmail.com>
2023-07-16 17:52:39 -0400

    Updated Position of NFT name

--- src/Components/ImageDisplay.tsx
+++ src/Components/ImageDisplay.tsx
@@ -5,15 +5,20 @@ const ImageDisplay = () => {
 
   if (listOfImages.length < 1) return null;
 
-  const testImage = listOfImages[50].image;
+  const testImage = listOfImages[333].image;
+  const testImage2 = listOfImages[222].image;
 
   return (
-    <div>
-      <div className='grid grid-cols-2'>
+    <div className='flex items-center justify-center '>
+      <div className='grid grid-cols-2 gap-2 mt-4'>
         <img className='h-auto w-48 rounded' src={testImage} alt='test' />
-        <img className='h-auto w-48 rounded' src={testImage} alt='test' />
-        <div>Mad Lad 1</div>
-        <div>Mad Lad 2</div>
+        <img className='h-auto w-48 rounded' src={testImage2} alt='test' />
+        <div className='flex items-center justify-center'>
+          <div>Mad Lad 1</div>
+        </div>
+        <div className='flex items-center justify-center'>
+          <div>Mad Lad 2</div>
+        </div>
       </div>
     </div>
   );
