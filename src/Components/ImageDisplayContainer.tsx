0f48301236f7ff033c852cf2842067137bbb9fbf
aych <aychdev1@gmail.com>
2023-07-18 16:42:39 -0400

    Added Title Display component

--- src/Components/ImageDisplayContainer.tsx
+++ src/Components/ImageDisplayContainer.tsx
@@ -1,9 +1,14 @@
 import FirstImageDisplay from './FirstImageDisplay';
 import SecondImageDispay from './SecondImageDispay';
+import TitleDisplay from './TitleDisplay';
+
 const ImageDisplay = () => {
   return (
-    <div className='flex items-center justify-center '>
+    <div className='flex items-center justify-center h-screen'>
       <div className='grid grid-cols-2 gap-2 mt-4'>
+        <div className='col-span-2'>
+          <TitleDisplay />
+        </div>
         <FirstImageDisplay />
         <SecondImageDispay />
       </div>
---
+++ src/Components/TitleDisplay.tsx
@@ -0,0 +1,9 @@
+const TitleDisplay = () => {
+  return (
+    <div className='flex items-center justify-center'>
+      Which NFT looks better?
+    </div>
+  );
+};
+
+export default TitleDisplay;
