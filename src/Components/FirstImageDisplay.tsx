c1edf2658bf4b14d1c6d5944beb95d9202051096
aych <aychdev1@gmail.com>
2023-07-19 17:55:12 -0400

    Added selectNewImage function to FirstImageDisplay

--- src/Components/FirstImageDisplay.tsx
+++ src/Components/FirstImageDisplay.tsx
@@ -1,22 +1,31 @@
+import { useState } from 'react';
 import useMintlist from '../Hooks/useMintlist';
 import MadLadsLogoBlack from '../Images/MadLads_BlackLogo.png';
 
 const FirstImageDisplay = () => {
   const { listOfImages } = useMintlist();
+  const [newImage, setNewImage] = useState(MadLadsLogoBlack);
+  const [newNumber, setNewNumber] = useState(0);
 
   if (listOfImages.length < 1) return null;
 
   const firstImageElement = listOfImages.map((image, index) => {
-    let randomNumber = 6;
+    const selectNewImage = () => {
+      setNewNumber(Math.floor(Math.random() * (1000 - 1 + 1) + 1));
+      if (newNumber === index) {
+        setNewImage(image.image);
+      }
+    };
 
-    if (randomNumber !== index) return null;
+    let number = newNumber;
+    if (number !== index) return null;
 
     return (
       <div key={image.id}>
         <img
-          onClick={() => console.log('test')}
+          onClick={() => selectNewImage()}
           className='h-auto w-48 rounded cursor-pointer'
-          src={MadLadsLogoBlack}
+          src={newImage}
           alt='test'
         />
         <div className='flex items-center justify-center'>
