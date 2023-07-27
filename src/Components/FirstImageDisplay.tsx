3487868c2a6937f63b295507e1c589b21025cd73
aych <aychdev1@gmail.com>
2023-07-26 23:28:04 -0400

    Solved bug to allow user to reset image to play game again

--- src/Components/FirstImageDisplay.tsx
+++ src/Components/FirstImageDisplay.tsx
@@ -1,35 +1,33 @@
 import { useEffect, useState } from 'react';
 import useMintlist from '../Hooks/useMintlist';
 import MadLadsLogoBlack from '../Images/MadLads_BlackLogo.png';
+import StartButton from './StartButton';
 
-interface Props {
-  startProgram: boolean;
-  firstPreLoadedImage: string;
-}
-
-const FirstImageDisplay = ({ startProgram, firstPreLoadedImage }: Props) => {
+const FirstImageDisplay = () => {
   const { listOfImages } = useMintlist();
-  const [preLoadedImage, setPreLoadedImage] =
-    useState<string>(firstPreLoadedImage);
-  const [preLoadedImageLabel, setPreLoadedImageLabel] =
-    useState<string>('Mad Lads #9308');
+  const [preLoadedImage, setPreLoadedImage] = useState<string>(
+    'https://madlads.s3.us-west-2.amazonaws.com/images/9308.png'
+  );
   const [preLoadedId, setPreLoadedId] = useState<string>('112222');
   const [newNumber, setNewNumber] = useState<number>(0);
   const [currentImageLabel, setCurrentImageLabel] =
-    useState<string>(preLoadedImageLabel);
+    useState<string>('Mad Lads #9108');
   const [currentImage, setCurrentImage] = useState<string>(
     'https://madlads.s3.us-west-2.amazonaws.com/images/9108.png'
   );
   const [currentId, setCurrentId] = useState<string>('abcdefg');
   const [imageSelected, setImageSelected] = useState<boolean>(false);
-
+  const [startProgram, setStartProgram] = useState<boolean>(false);
+  const [firstPreLoadedImage, setFirstPreLoadedImage] = useState<string>(
+    'https://madlads.s3.us-west-2.amazonaws.com/images/9308.png'
+  );
   useEffect(() => {
     const imagePreLoader = () => {
       setNewNumber(Math.floor(Math.random() * 1000) + 1);
       listOfImages.map((image, index) => {
         if (newNumber === index) {
           setPreLoadedImage(image.image);
-          setPreLoadedImageLabel(image.name);
+          // setPreLoadedImageLabel(image.name);
           setPreLoadedId(image.id);
         }
       });
@@ -37,13 +35,21 @@ const FirstImageDisplay = ({ startProgram, firstPreLoadedImage }: Props) => {
     imagePreLoader();
   }, [imageSelected]);
 
-  const selectedImage = () => {
-    if (imageSelected === false) {
-      setImageSelected(!imageSelected);
+  const onStartProgram = () => {
+    setStartProgram((prevState) => !prevState);
+    if (startProgram) {
+      setCurrentImage(
+        'https://madlads.s3.us-west-2.amazonaws.com/images/9108.png'
+      );
     }
-    setCurrentImage(preLoadedImage);
-    setCurrentImageLabel(preLoadedImageLabel);
-    setCurrentId(preLoadedId);
+    console.log('test');
+  };
+
+  const selectedImage = async () => {
+    await setImageSelected(!imageSelected);
+    await setCurrentImage(preLoadedImage);
+    // await setCurrentImageLabel(preLoadedImageLabel);
+    await setCurrentId(preLoadedId);
   };
 
   if (startProgram) {
@@ -58,18 +64,30 @@ const FirstImageDisplay = ({ startProgram, firstPreLoadedImage }: Props) => {
         <div className='flex items-center justify-center'>
           <div>{currentImageLabel}</div>
         </div>
+        <div className='flex items-center justify-center col-span-2 mt-10'>
+          <StartButton
+            onStartProgram={onStartProgram}
+            startProgram={startProgram}
+          />
+        </div>
       </div>
     );
   } else {
     return (
       <div key={currentId}>
         <img
-          className='h-auto w-48 rounded cursor-pointer'
+          className='h-auto w-48 rounded '
           src={MadLadsLogoBlack}
           alt='test'
         />
         <div className='flex items-center justify-center'>
           <div>Mad Lads</div>
+        </div>
+        <div className='flex items-center justify-center col-span-2 mt-10'>
+          <StartButton
+            onStartProgram={onStartProgram}
+            startProgram={startProgram}
+          />
         </div>
       </div>
     );
--- src/Components/ImageDisplayContainer.tsx
+++ src/Components/ImageDisplayContainer.tsx
@@ -6,29 +6,6 @@ import StartButton from './StartButton';
 import TitleDisplay from './TitleDisplay';
 
 const ImageDisplay = () => {
-  const [startProgram, setStartProgram] = useState<boolean>(false);
-  const [firstPreLoadedImage, setFirstPreLoadedImage] = useState<string>(
-    'https://madlads.s3.us-west-2.amazonaws.com/images/9308.png'
-  );
-  const [secondPreLoadedImage, setSecondPreLoadedImage] = useState<string>(
-    'https://madlads.s3.us-west-2.amazonaws.com/images/9798.png'
-  );
-  const [secondPreLoadedLabel, setSecondPreLoadedLabel] =
-    useState<string>('Mad Lads #9798');
-
-  // Starts the program
-  const onStartProgram = () => {
-    setStartProgram((prevState) => !prevState);
-    if (startProgram === false) {
-      setFirstPreLoadedImage(
-        'https://madlads.s3.us-west-2.amazonaws.com/images/9308.png'
-      );
-      setSecondPreLoadedImage(
-        'https://madlads.s3.us-west-2.amazonaws.com/images/9798.png'
-      );
-    }
-  };
-
   return (
     <div className='flex items-center justify-center h-screen'>
       <div className='grid grid-cols-2 gap-2 '>
@@ -36,29 +13,19 @@ const ImageDisplay = () => {
           <TitleDisplay />
         </div>
         <div className='flex items-center justify-center'>
-          <FirstImageDisplay
-            firstPreLoadedImage={firstPreLoadedImage}
-            startProgram={startProgram}
-          />
+          <FirstImageDisplay />
         </div>
         <div className='flex items-center justify-center'>
-          <SecondImageDispay
+          {/* <SecondImageDispay
             secondPreLoadedImage={secondPreLoadedImage}
-            secondPreLoadedLabel={secondPreLoadedLabel}
             startProgram={startProgram}
-          />
+          /> */}
         </div>
         <div className='flex items-center justify-center'>
           <RatingTracker />
         </div>
         <div className='flex items-center justify-center'>
           <RatingTracker />
-        </div>
-        <div className='flex items-center justify-center col-span-2 mt-10'>
-          <StartButton
-            onStartProgram={onStartProgram}
-            startProgram={startProgram}
-          />
         </div>
       </div>
     </div>
