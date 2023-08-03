fcab2a1eac7562c98d40b073314fffc8a7edfb48
aych <aychdev1@gmail.com>
2023-08-03 17:08:27 -0400

    Updated server to fetch the entire NFT collection

--- Services/server.js
+++ Services/server.js
@@ -10,25 +10,37 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', async (req, res) => {
-  try {
-    const url = `https://rpc.helius.xyz/?api-key=${process.env.API_KEY}`;
-    const { data } = await axios.post(url, {
-      jsonrpc: '2.0',
-      id: 'my-id',
-      method: 'getAssetsByGroup',
-      params: {
-        groupKey: 'collection',
-        groupValue: 'J1S9H3QjnRtBbbuD4HjPV6RpRhwuk4zKbxsnCHuTgh9w',
-        page: 1,
-        limit: 1000,
-      },
-    });
-    const { result } = data;
-    res.json(result.items);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Internal server error!!' });
+  let page = 1;
+  let assetList = [];
+
+  while (page) {
+    try {
+      const url = `https://rpc.helius.xyz/?api-key=${process.env.API_KEY}`;
+      const { data } = await axios.post(url, {
+        jsonrpc: '2.0',
+        id: 'my-id',
+        method: 'getAssetsByGroup',
+        params: {
+          groupKey: 'collection',
+          groupValue: 'J1S9H3QjnRtBbbuD4HjPV6RpRhwuk4zKbxsnCHuTgh9w',
+          page: page,
+          limit: 1000,
+        },
+      });
+      const { result } = await data;
+      assetList.push(...result.items);
+
+      if (result.total !== 1000) {
+        page = false;
+      } else {
+        page++;
+      }
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Internal server error!!' });
+    }
   }
+  res.json(assetList);
 });
 
 app.listen(PORT, () => console.log(`Server is running ${PORT}`));
