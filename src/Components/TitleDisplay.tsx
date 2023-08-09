1ecf1eecfeae2b723fafab1543b93878817f1ca7
aych <aychdev1@gmail.com>
2023-08-09 18:40:27 -0400

    Updated Title Size

--- src/Components/TitleDisplay.tsx
+++ src/Components/TitleDisplay.tsx
@@ -1,6 +1,8 @@
 const TitleDisplay = () => {
   return (
-    <div className='flex items-center justify-center mb-4'>FOCKIN' Choose</div>
+    <div className='flex items-center justify-center mb-4 font-bold font-sans-pro text-xl'>
+      FOCKIN' Choose
+    </div>
   );
 };
 
