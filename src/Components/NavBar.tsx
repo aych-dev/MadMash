992509b7c60f42b8dcfd920fc136486bc10435b4
aych <aychdev1@gmail.com>
2023-08-15 17:22:09 -0400

    Added links to navbar

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,10 +1,22 @@
 import RedMadLadLogo from '../Images/MadLads_RedLogo.png';
+import { Breadcrumbs, Link } from '@mui/material';
 
 const NavBar = () => {
   return (
-    <div className='flex justify-start  border border-black rounded-b bg-gradient-to-r to-customRed from-black'>
-      <img src={RedMadLadLogo} alt='Mad Lads' className='h-20 ml-2' />
-    </div>
+    <>
+      <div className='flex items-center justify-between border border-black rounded-b bg-gradient-to-r to-customRed from-black'>
+        <div>
+          <img src={RedMadLadLogo} alt='Mad Lads' className='h-20 ml-2' />
+        </div>
+        <div className='mr-2'>
+          <Breadcrumbs>
+            <Link>one</Link>
+            <Link>two</Link>
+            <Link>three</Link>
+          </Breadcrumbs>
+        </div>
+      </div>
+    </>
   );
 };
 
